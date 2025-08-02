76012cc8dd93d7fb9fc5efefe51d0a8e88219199
FireDasher <150294350+FireDasher@users.noreply.github.com>
2025-08-02 11:10:19 -0700

    made website
    Co-authored-by: ElectricalOutlet <ChloPUPs@users.noreply.github.com>

---
+++ dkljhfasd.js
@@ -0,0 +1,35 @@
+function easeInOutCubic(t) {
+    return t < 0.5 ? 4 * t * t * t : 1 - Math.pow(-2 * t + 2, 3) / 2;
+}
+
+function lerp(a, b, t) {
+    return a + (b - a) * t;
+}
+
+document.addEventListener("mousemove", e=>{
+    document.querySelectorAll(".title > span").forEach(ee=>{
+        let scale = 1;
+
+        const elementRect = ee.getBoundingClientRect();
+
+        const elementCenterX = elementRect.left + elementRect.width / 2;
+        const elementCenterY = elementRect.top + elementRect.height / 2;
+
+        const distanceX = e.clientX - elementCenterX;
+        const distanceY = e.clientY - elementCenterY;
+        const dist = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+
+        console.log(dist);
+
+        if (dist < 150) {
+            //sayToothpaste();
+            scale = lerp(1, 1.5, easeInOutCubic((150 - dist) / 150));
+        }
+
+        ee.style.scale = scale;
+    });
+});
+
+function sayToothpaste() {
+    alert("Toothpaste!!")
+}
